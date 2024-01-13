46b9258210520d7095b7ecf80f3cc690f3b88f50
Leonardo Rodrigues Gomes <leonardo.r.gomes@Hotmail.com>
2024-01-13 19:34:28 -0300

    Style home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,62 @@
-import { HomeContainer, Title } from './styles'
-
+import { Benefits, HomeContainer, RoundedIcon, Title } from './styles'
+import HomeImage from '../../assets/Imagem.png'
+import { Coffee, Package, ShoppingCart, Timer } from 'phosphor-react'
 const Home = () => {
   return (
-    <HomeContainer>
-      <Title>Encontre o cafe perfeito para qualquer hora do dia</Title>
-      <p>
-        Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora
-      </p>
-    </HomeContainer>
+    <div>
+      <HomeContainer>
+        <div>
+          <Title>Encontre o cafe perfeito para qualquer hora do dia</Title>
+          <p>
+            Com o Coffee Delivery você recebe seu café onde estiver, a qualquer
+            hora
+          </p>
+        </div>
+
+        <img src={HomeImage} alt="Coffee image" />
+      </HomeContainer>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          gap: '7rem',
+          alignItems: 'center',
+        }}
+      >
+        <div style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
+          <div style={{ display: 'flex', alignItems: 'center', gap: '0.5rem' }}>
+            <RoundedIcon statuscolor="yellow-dark">
+              {' '}
+              <ShoppingCart size={24} weight="fill" color="white" />
+            </RoundedIcon>
+            <Benefits>Compra simples e segura</Benefits>
+          </div>
+          <div style={{ display: 'flex', alignItems: 'center', gap: '0.5rem' }}>
+            <RoundedIcon statuscolor="yellow-normal">
+              {' '}
+              <Timer size={24} weight="fill" color="white" />
+            </RoundedIcon>
+            <Benefits>Entrega rápida e rastreada</Benefits>
+          </div>
+        </div>
+        <div style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
+          <div style={{ display: 'flex', alignItems: 'center', gap: '0.5rem' }}>
+            <RoundedIcon statuscolor="base-text">
+              {' '}
+              <Package size={24} weight="fill" color="white" />
+            </RoundedIcon>
+            <Benefits>Compra simples e segura</Benefits>
+          </div>
+          <div style={{ display: 'flex', alignItems: 'center', gap: '0.5rem' }}>
+            <RoundedIcon statuscolor="purple-normal">
+              {' '}
+              <Coffee size={24} weight="fill" color="white" />
+            </RoundedIcon>
+            <Benefits>Entrega rápida e rastreada</Benefits>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,7 +1,20 @@
 import styled from 'styled-components'
 
 export const HomeContainer = styled.div`
-  margin-top: 3rem;
+  margin-top: 4rem;
+  display: flex;
+  gap: 0.5rem;
+  align-items: center;
+
+  p {
+    font-family: 'Roboto', sans-serif;
+    width: 70%;
+
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 130%;
+    margin-top: 1rem;
+  }
 `
 
 export const Title = styled.h1`
@@ -9,9 +22,40 @@ export const Title = styled.h1`
   font-weight: 800;
   line-height: 62.4px;
   font-size: 48px;
-  width: 50%;
+  width: 80%;
+`
 
-  p {
-    font-family: 'Roboto', sans-serif;
-  }
+const STATUS_COLORS = {
+  'yellow-normal': '#DBAC2C',
+  'yellow-light': '#F1E9C9',
+  'yellow-dark': '#C47f17',
+
+  'purple-normal': '#8047F8',
+  'purple-light': '#EBE5F9',
+  'purple-dark': '#4B2995',
+  'base-text': '#574F4D',
+} as const
+
+interface StatusProps {
+  statuscolor: keyof typeof STATUS_COLORS
+}
+
+export const RoundedIcon = styled.div<StatusProps>`
+  background-color: ${(props) => props.theme.colors[props.statuscolor]};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.2rem;
+
+  padding: 0.5rem;
+  border-radius: 50%;
+
+  font-size: 0.875rem;
+  width: 2rem;
+  height: 2rem;
+`
+
+export const Benefits = styled.p`
+  font-size: 14px;
+  color: ${(props) => props.theme.colors['base-text']};
 `
